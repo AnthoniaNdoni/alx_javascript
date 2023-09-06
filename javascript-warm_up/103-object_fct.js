805f4d6f97a76154fce5fea68d6a82adc61f8a57
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-07 00:57:56 +0100

    correctig commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,7 +1,14 @@
-/**
- * a file
- * the function return the plus of 
- * of two files in JS
- */
-
-exports.add = (a, b) => a + b;
+// Function to create an object with type and value properties
+function createObject(value, incrFunction) {
+    const obj = { type: 'object', value };
+    if (incrFunction) {
+      obj.incr = incrFunction;
+    }
+    return obj;
+  }
+  
+  // Function to increment the value property of an object
+  function increment(obj) {
+    obj.value += 1;
+    return obj;
+  }
