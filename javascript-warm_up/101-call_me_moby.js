283f319dd666c8a1f1f59ef64bf9f86e0e574157
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-06 22:35:50 +0100

    commit

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,15 @@
-/**
- * A funstion tha execute
- * x times a funxtion
- */
-
-function callMeMoby(x, theFunction){
-    return x * theFunction;
-}
+// Define a global object to store functions
+if (typeof globalFunctions === 'undefined') {
+    globalFunctions = {};
+  }
+  
+  /**
+   * A function that executes another function 'x' times.
+   * @param {number} x - The number of times to execute the function.
+   * @param {function} theFunction - The function to be executed.
+   */
+  globalFunctions.callMeXTimes = function(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+      theFunction();
+    }
+  };
