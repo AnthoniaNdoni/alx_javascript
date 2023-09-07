fa5cc02c168abd13f88eeaddf345b264121e7b31
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-07 18:20:05 +0100

    commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -17,3 +17,8 @@ let obj1 = createObject(12);
 let obj2 = createObject(13, () => increment(obj2));
 let obj3 = createObject(14, () => increment(obj3));
 let obj4 = createObject(15, () => increment(obj4));
+
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+console.log(obj4);
