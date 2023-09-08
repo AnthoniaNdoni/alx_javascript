d6eeebc9bd3d8dbd9ec8c0a60d64dfdc7943c453
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-08 22:04:35 +0100

    correcting commit

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -5,20 +5,21 @@
 module.exports = class Rectangle {
     constructor(w, h) {
         if(w <= 0 || h <= 0 || h == undefined || w == undefined) {
-            return null;
+            throw new Error("Invalid width or height values.");
         } else {
             this.width = w;
             this.height = h;
         }
     }
     
-    // print square from recttangle
+    // Print a square of asterisks
     print() {
         for (let i = 0; i < this.height; i++) {
+            let row = '';
             for (let j = 0; j < this.width; j++) {
-                console.log('*');
-            };
-            console.log('');
-        };
+                row += '* ';
+            }
+            console.log(row);
+        }
     }
 }
