875ad6c73ee6893eab48d19c554e0c5188f1bfba
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-08 20:57:18 +0100

    Initial commit

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+class Rectangle {
+    // The class is empty, with no attributes or methods defined.
+  }
+  
+  // You can create instances of the Rectangle class later when needed.
+  const myRectangle = new Rectangle();
