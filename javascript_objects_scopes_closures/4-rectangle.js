03d233ca196c336cc42a779389447ae0cb223784
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-09 02:41:12 +0100

    commit

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -5,7 +5,7 @@
 module.exports = class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
-            throw new Error("Invalid width or height values.");
+            return null;
         } else {
             this.width = w;
             this.height = h;
