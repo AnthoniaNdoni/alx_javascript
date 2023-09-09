e6151a6f0399cb68e58c0c21818bb67bdf24cda4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-09 01:25:34 +0100

    Initial commit

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,28 +2,20 @@
 
 //create a square of w and h
 
-  module.exports = class Rectangle {
-      constructor(w, h) {
-          if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
-              throw new Error("Invalid width or height values.");
-          } else {
-              this.width = w;
-              this.height = h;
-          }
-      }
-  
-      // Print a rectangle of 'X' characters
-      print() {
-          console.log(`Rectangle { width: ${this.width}, height: ${this.height} }`);
-          console.log(this.width);
-          console.log(this.height);
-  
-          for (let i = 0; i < this.height; i++) {
-              let row = '';
-              for (let j = 0; j < this.width; j++) {
-                  row += 'X ';
-              }
-              console.log(row);
-          };
-      }
-  }
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
+            throw new Error("Invalid width or height values.");
+        } else {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    // Print a rectangle of 'X' characters
+    print() {
+        for (let i = 0; i < this.height; i++) {
+             console.log('X'.repeat(this.height));
+        };
+    }
+}       
