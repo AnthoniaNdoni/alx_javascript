6808de5530af694ce609c7ce657a9b52082b515f
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-11 18:46:15 +0100

    correcting commit

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,8 +2,12 @@
 
 const request = require('request');
 
-const url = process.argv[2];
-request.get (url);
-    .on('response', response=> {
-        console.log('code: ${response.statuscode}');
-    });
+const url = process.argv[2]; // Retrieve the URL from the command line arguments
+
+request.get(url, (error, response) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(`code: ${response.statusCode}`);
+    }
+});
