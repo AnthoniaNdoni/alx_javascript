f0d8e12fcb6cb100c63fbe5ad91df0be12ed9b15
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-12 22:31:11 +0100

    Initial commit

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+// Get the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+// Character ID for Wedge Antilles
+const characterId = 18;
+
+// Send a GET request to the API
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error(`HTTP Status Code: ${response.statusCode}`);
+    } else {
+        try {
+            const filmsData = JSON.parse(body).results;
+            const moviesWithWedgeAntilles = filmsData.filter((film) =>
+                film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+            );
+            console.log(`Number of movies with Wedge Antilles: ${moviesWithWedgeAntilles.length}`);
+        } catch (parseError) {
+            console.error('Error parsing JSON:', parseError);
+        }
+    }
+});
