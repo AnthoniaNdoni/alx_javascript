c431051fb9bc2abba556b07c44d1d8e60dbf1b7b
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-13 18:15:36 +0100

    commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,26 +1,8 @@
 #!/usr/bin/node
-
+const url = process.argv[2];
 const request = require('request');
 const fs = require('fs');
 
-// Get the URL and file path from command line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
 
-// Send a GET request to the URL
-request.get(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error(`HTTP Status Code: ${response.statusCode}`);
-    } else {
-        // Write the response body to a file with UTF-8 encoding
-        fs.writeFile(filePath, body, { encoding: 'utf-8' }, (writeError) => {
-            if (writeError) {
-                console.error('Error writing to file:', writeError);
-            } else {
-                console.log(`Web content saved to ${filePath}`);
-            }
-        });
-    }
-});
+request(url)
+  .pipe(fs.createWriteStream(process.argv[3]));
