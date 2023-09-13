7b471faf734e745aa18c86af7e2aa74b51481760
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-13 01:47:08 +0100

    commiting the task 3 Loripsumm

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,14 +1,25 @@
 #!/usr/bin/node
-const url = process.argv[2];
-const request = require('request');
+onst request = require('request');
 const fs = require('fs');
 
+// Get the URL and file path from command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-const result = request.get(url, {encoding:'utf-8'});
-    
-if(process.argv > 2) {
-    const path_to_store = process.agrv[3];
-    result.pipe(fs.WriteStream(path_to_store));
-}else {
-    console.log(result);
-}
+// Send a GET request to the URL
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error(`HTTP Status Code: ${response.statusCode}`);
+    } else {
+        // Write the response body to a file with UTF-8 encoding
+        fs.writeFile(filePath, body, { encoding: 'utf-8' }, (writeError) => {
+            if (writeError) {
+                console.error('Error writing to file:', writeError);
+            } else {
+                console.log(`Web content saved to ${filePath}`);
+            }
+        });
+    }
+});
