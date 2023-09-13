9d6f9d229e37429d32f4ac72a24863972bb8e9fc
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-13 19:03:30 +0100

    initial commit for task 2 Star wars wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,28 +1,8 @@
 #!/usr/bin/node
-
+const url = process.argv[2];
 const request = require('request');
+const fs = require('fs');
 
-// Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
 
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Send a GET request to the API
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error(`HTTP Status Code: ${response.statusCode}`);
-    } else {
-        try {
-            const filmsData = JSON.parse(body).results;
-            const moviesWithWedgeAntilles = filmsData.filter((film) =>
-                film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-            );
-            console.log(moviesWithWedgeAntilles.length); // Print only the number of movies
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-        }
-    }
-});
+request(url)
+  .pipe(fs.createWriteStream(process.argv[3]));
