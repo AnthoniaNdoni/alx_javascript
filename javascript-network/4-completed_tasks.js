3bc8f40478adcf1532585818d4c523eeb7607f68
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-13 16:13:45 +0100

    Correction of initial commit

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,46 +1,20 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+
 const request = require('request');
 
-// Define the API URL
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
-// Send a GET request to the API
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error(`HTTP Status Code: ${response.statusCode}`);
-    } else {
-        try {
-            const todos = JSON.parse(body);
-
-            // Create an object to store completed task counts for each user
-            const completedTasksByUser = {};
-
-            // Iterate through the todos
-            todos.forEach((todo) => {
-                if (todo.completed) {
-                    // If the task is completed, increment the count for the user
-                    const userId = todo.userId;
-                    if (completedTasksByUser[userId]) {
-                        completedTasksByUser[userId]++;
-                    } else {
-                        completedTasksByUser[userId] = 1;
-                    }
-                }
-            });
-
-            // Filter users with at least 3 completed tasks
-            const filteredResults = {};
-            for (const userId in completedTasksByUser) {
-                if (completedTasksByUser[userId] >= 3) {
-                    filteredResults[userId] = completedTasksByUser[userId];
-                }
+request.get(url, (error, response, body) => {
+    const todos = JSON.parse(body);
+    let completed_task = {}
+    if(!error && response.statusCode == 200) {
+        todos.forEach((todo) => {
+            if (todo.completed && completed_task[todo.userId] === undefined) {
+                completed_task[todo.userId]  = 1;
+            }else if(todo.completed){
+                completed_task[todo.userId] += 1;
             }
-
-            // Print the filtered results as an object
-            console.log(JSON.stringify(filteredResults));
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-        }
+        })
     }
+    console.log(completed_task)
 });
