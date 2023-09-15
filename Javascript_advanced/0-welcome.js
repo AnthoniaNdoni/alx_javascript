b89303a96b72ea53d5ae5a916f78a49f74084d15
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-16 00:10:34 +0100

    initial commit

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,14 +1,20 @@
-// function with inner function call
 function welcome(firstName, lastName) {
-    // set fullname = first and last name
-    let fullName = `${firstName} ${lastName}`;
-
-    // define a funtion for full name display
-    function displayFullName(){
-
-        // display an alert of full name
-        alert(`Welcome ${fullName}!`);
+    // Concatenate first name and last name with a space in between
+    const fullName = firstName + ' ' + lastName;
+  
+    // Define a function named displayFullName
+    function displayFullName() {
+      // Display an alert with the welcome message
+      alert('Welcome ' + fullName + '!');
     }
-    // call the display function
+  
+    // Call the displayFullName function
     displayFullName();
-}
+  }
+  
+  // Call the welcome function with 'Holberton' and 'School' as arguments
+  welcome('Holberton', 'School');
+  
+  
+  
+  
