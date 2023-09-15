27fcbfbb776486a9e1a6ffc2a538a1b35dd3154c
AnthoniaNdoni <tamarandoni@gmail.com>
2023-09-15 16:22:02 +0100

    correcting star wars wedge Antilles task 2 commit

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,28 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Send a GET request to the API
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error(`HTTP Status Code: ${response.statusCode}`);
-    } else {
-        try {
-            const filmsData = JSON.parse(body).results;
-            const moviesWithWedgeAntilles = filmsData.filter((film) =>
-                film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-            );
-            console.log(moviesWithWedgeAntilles.length); // Print only the number of movies
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-        }
+request.get(url, (error, response, body) =>{
+    if(!error && response.statusCode == 200){
+        const films = JSON.parse(body);
+        let counter = 0;
+        films.results.forEach((film) =>{
+            film.characters.forEach((actor) => {
+                const split_film = actor.split('/');
+                if(split_film[split_film.length-2] == 18) {
+                    counter += 1;
+                }
+            })
+        })
+        console.log(counter);
     }
 });
